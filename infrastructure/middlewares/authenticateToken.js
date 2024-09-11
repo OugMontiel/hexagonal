62f7c6f1228147eab4c205e7a6fde2d59cf24178
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-09-11 17:52:45 -0500

    feat: :zap: middlewares auth agregado
    aun no aplicado devido a no enntender el funcionamiento

---
+++ infrastructure/middlewares/authenticateToken.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+exports.auth =(req, res, next) =>{
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({message: 'token no proporcionado'});
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) =>{
+        if (err) return res.status(403).json({message:'Token Invalido'});
+        next()
+    });
+}   
