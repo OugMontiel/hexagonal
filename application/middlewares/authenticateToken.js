8eb9c44878f9ea39c00907107ae31ae2f38c10d2
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-09-12 17:39:57 -0500

    feat: :truck: Move dir del Token para Ubicacion adeciada

--- application/middlewares/authenticateToken.js
+++ application/middlewares/authenticateToken.js
@@ -5,7 +5,7 @@ exports.auth =(req, res, next) =>{
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.status(401).json({message: 'token no proporcionado'});
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, payload) =>{
+    jwt.verify(token, process.env.KEY_SECRET, (err, payload) =>{
         if (err) return res.status(403).json({message:'Token Invalido'});
         next()
     });
