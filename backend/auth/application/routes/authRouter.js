fe0b95e2027fb95e763815fb5decaf55a1d1cac4
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-09-17 16:25:44 -0500

    feat: :sparkles: Contruccion de rutas
    Aun sin contenido mas alla del que vemos .. y primero vamoos por la ccokie

---
+++ backend/auth/application/routes/authRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const passport = require('passport');
+const AuthController = require('../controllers/authController'); // Importa el controlador de autenticación.
+const AuthValidator = require('../validator/authValidator'); 
+
+// enrutador de Express para manejar las rutas específicas de autenticación.
+const router = express.Router();
+const authController = new AuthController();
+const authValidator = new AuthValidator();
+
+// Define la ruta para iniciar sesión mediante cookies.
+router.post('/sessionLogin', authValidator.validatorSessionLogin(), (req, res) =>
+  authController.sessionLogin(req, res)
+);
+
+// Define la ruta para iniciar sesión mediante sesión Express.
+router.post('/sessionLogin', authValidator.validatorSessionLogin(), (req, res) =>
+    authController.sessionLogin(req, res)
+);
+
+// Ruta para iniciar sesión con Google
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect('/'); // Redirige a la página principal o donde desees
+});
+
+// Ruta para iniciar sesión con Facebook
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+router.get('/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+  res.redirect('/'); // Redirige a la página principal o donde desees
+});
+
+// Ruta para iniciar sesión con Discord
+router.get('/discord', passport.authenticate('discord', { scope: ['identify', 'email'] }));
+router.get('/discord/callback', passport.authenticate('discord'), (req, res) => {
+  res.redirect('/'); // Redirige a la página principal o donde desees
+});
+
+// Exporta el enrutador para que pueda ser utilizado en otras partes de la aplicación.
+module.exports = router;
